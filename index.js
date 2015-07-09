0bc7e0b3b11d04fd26a8334822e6a33368ecc983
orggue <jorgechato1@gmail.com>
2015-07-09 20:08:51 +0200

    improvement in response time

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 var express = require('express'),
-swig = require('swig'),
 cors = require('cors'),
+compression = require('compression'),
 bodyParser = require('body-parser');
 
 var app = express();
 
 //Config views
-app.engine('html',swig.renderFile);
 app.set('view engine','html');
 app.set('views','./dist/views');
 
 app.set('port', (process.env.PORT || 5000));
 
 //Cors and init json Middleware
-var corsOptions = {
-  origin: 'http://google.com'
-};
 app.use(bodyParser.json('application/json'));
 app.use(cors());
 
 //Static files
+app.use(compression());
 app.use(express.static('./dist'));
 
 var config = require('./lib/config');
-//app.use(config);
-
-app.get('/',function(req, res){
-  res.render('index');
-  config.getLogger('test','hola test');
-});
 
 var twitter = require('./lib/twitter');
 app.use(twitter);
